c70a242af0f36357d23589153cb3ab228437dcaf
Severin Mairinger <sevm@pm.me>
2021-12-06 14:35:16 +0100

    reformatted "one-liner"

--- aoc_3/aoc_3t2.js
+++ aoc_3/aoc_3t2.js
@@ -1,7 +1,1 @@
-require('fs/promises').readFile('./input.txt').then(data => data.toString().split('\n').map(e => e.split('').map(e => +e))).then(data => {
-    data.splice(data.length - 1, 1);
-    function fv(arr, depth = 0, mcb = true) {
-        return (arr.length === 1) ? arr : fv(arr.filter(e => e[depth] ===  (arr.reduce((a, c) => a + c[depth], 0) >= (arr.length / 2) ^ !mcb)), ++depth, mcb)
-    }
-    return [fv(data), fv(data, 0, false)];
-}).then(arr => arr.map(arr => parseInt(arr.toString().replaceAll(',', ''), 2)).reduce( (a, c) => a * c, 1)).then(console.log)
+require('fs/promises').readFile('./input.txt').then(data => data.toString().split('\n').map(e => e.split('').map(e => +e))).then(data => {data.splice(data.length - 1, 1);function fv(arr, depth = 0, mcb = true) {return (arr.length === 1) ? arr : fv(arr.filter(e => e[depth] ===  (arr.reduce((a, c) => a + c[depth], 0) >= (arr.length / 2) ^ !mcb)), ++depth, mcb)}return [fv(data), fv(data, 0, false)];}).then(arr => arr.map(arr => parseInt(arr.toString().replaceAll(',', ''), 2)).reduce( (a, c) => a * c, 1)).then(console.log)
