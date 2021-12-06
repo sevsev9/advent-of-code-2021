c331e9bb075431bf8ef4f5e98d07c6d255350d84
Severin Mairinger <sevm@pm.me>
2021-12-06 14:34:40 +0100

    added aoc3

--- aoc_2/aoc_2t1.js
+++ aoc_2/aoc_2t1.js
@@ -1,1 +1,15 @@
-require('fs/promises').readFile('input.txt').then(buf => buf.toString().split('\n')).then(data => data.reduce((acc, curr) => {(curr.startsWith('f')) ? acc[0] += curr.slice(-1) - 0 : acc[1] += ((curr.startsWith('u')) ? 0 - curr.slice(-1) : curr.slice(-1) - 0); return acc;}, [0, 0])).then(arr => arr[0] * arr[1]).then(console.log)
+require('fs/promises')
+    .readFile('input.txt')
+    .then(buf => buf.toString().split('\n'))
+    .then(data => data.reduce((acc, curr) => {
+        (curr.startsWith('f')) ?
+            acc[0] += curr.slice(-1) - 0
+            : acc[1] += (
+                (curr.startsWith('u')) ?
+                    0 - curr.slice(-1)
+                    : curr.slice(-1) - 0
+            );
+        return acc;
+        }, [0, 0]))
+    .then(arr => arr[0] * arr[1])
+    .then(console.log)
